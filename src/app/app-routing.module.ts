f25e47a22b453eacb820490b13fbc48c06085ef1
Den <saloed2206@gmail.com>
2023-04-19 17:21:00 +0300

    added structure / add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductInfoComponent } from './components/product-info/product-info.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'products',
+    component: ProductsListComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductInfoComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
