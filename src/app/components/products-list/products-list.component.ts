01fd6f5cc0734f673e6fbbc85b985990073542b7
Den <saloed2206@gmail.com>
2024-11-06 20:29:08 +0200

    added filter by category

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { IProduct } from 'src/app/models/product.interface';
 import { ProductsService } from 'src/app/services/products.service';
@@ -9,22 +10,71 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  public categoryType = [
+    {
+      id: 0,
+      label: 'All',
+      name: "All"
+    },
+    {
+      id: 1,
+      label: 'Electronics',
+      name: "electronics"
+    },
+    {
+      id: 2,
+      label: 'Jewelery',
+      name: "jewelery"
+    },
+    {
+      id: 3,
+      label: "Men's clothing",
+      name: "women's clothing"
+    },
+    {
+      id: 4,
+      label: "Women's clothing",
+      name: "women's clothing"
+    },
+  ];
 
   productsList!: IProduct[];
   productsSubscription!: Subscription;
-  newProduct!: any;
+  filteredProducts: IProduct[] = [];
+
+  public form = this.fb.group({
+    category: [this.categoryType[0].name],
+  });
+
+  get categoryFC() {
+    return this.form.get('category')?.value;
+  }
 
   constructor(
     private productService: ProductsService,
+    private fb: FormBuilder
   ) { }
 
 
   ngOnInit(): void {
-    this.productsSubscription = this.productService.getProducts().subscribe((data: any) => {
+    this.productsSubscription = this.productService.getProducts().subscribe((data: IProduct[]) => {
       this.productsList = data;
-      console.log('this.productsList4', this.productsList)
+      this.applyFilter(String(this.categoryFC));
     });
-  };
+
+    this.form.valueChanges.subscribe(() => {
+      this.applyFilter(String(this.categoryFC));
+    });
+
+  }
+
+  applyFilter(categoryName: string): void {
+    if (categoryName === 'All') {
+      this.filteredProducts = this.productsList;
+    } else {
+      this.filteredProducts = this.productsList.filter(item => item.category === categoryName);
+    }
+  }
 
   ngOnDestroy() {
     if (this.productsSubscription) this.productsSubscription.unsubscribe();
